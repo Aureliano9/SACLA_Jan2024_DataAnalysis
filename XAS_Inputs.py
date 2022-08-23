fb108249a479f13780982016c4ea1ae0adde4e24
Aureliano9 <zhanatai1993@gmail.com>
2022-08-23 15:13:13 +0200

    Update XAS_Inputs.py

--- XAS_Inputs.py
+++ XAS_Inputs.py
@@ -3,6 +3,7 @@ import time
 start_time = time.time()
 
 import sorting_tool
+import sorting_tool_simple
 import numpy as np
 
 #Select the directory where the file is located, as well as TM data
@@ -12,24 +13,28 @@ SaveFolder = '/home/nurekeyev/nurekeyev/SavedResults/2022_08_23/'
 
 ExtraComment = 'test' #Add comment at the end of the generated file
 
-#Set some initial parameters
 FemtosecondInPls = 6.671 #Femtosends in 1 pls
 TimeZero = 1375 #estimated Time zero in pls
 
-BinsSize = [10,20,30,40,50] #Set the bin size for rebinning in fs
-BinsSize = np.array(BinsSize)
+BinSize = 25 #Set the bin size for rebinning in fs
+
 #Threshold for selecting the good bins. 
 #If the number of points in that bin is less than Bin_Threshold+1, then that bin is rejected
 Bin_Threshold = 0 
 
-#Select an optical attenuator value, 0 for 0%, -2500 for 50%
-Attenuation = 0
+#Select an optical attenuator value. 0 for 5 uJ of laser energy, -2500 for 2.9 uJ
+Attenuation = np.array([0, -2500])
 # Select the Run Number, in a loop if needed
 RunNumber = 1145445
 
-for i in range(len(BinsSize)):
-	BinSize = BinsSize[i]
-	XAS_sorting_tool.sorting_tool(RunNumber,DataDirectory,DataDirectoryTM,SaveFolder,ExtraComment,FemtosecondInPls,TimeZero,Attenuation,BinSize,Bin_Threshold)
+for i in Attenuation:
+	ExtraComment = i
+	sorting_tool.sorting_tool(RunNumber, DataDirectory, DataDirectoryTM, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, i, BinSize, Bin_Threshold)
+	sorting_tool_simple.sorting_tool_simple(RunNumber, DataDirectory, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, i)
+
+#Full possible inputs:
+#sorting_tool.sorting_tool(RunNumber,DataDirectory,DataDirectoryTM,SaveFolder,ExtraComment,FemtosecondInPls,TimeZero,Attenuation,BinSize,Bin_Threshold)
+#sorting_tool_simple.sorting_tool_simple(RunNumber, DataDirectory, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, Attenuation)
 
 print('done')
 print("--- %s seconds ---" % (time.time() - start_time))
