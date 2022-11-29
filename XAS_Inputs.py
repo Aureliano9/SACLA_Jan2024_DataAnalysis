0e42a30273e2df9287755297e6b32e2528e2422c
Aureliano9 <zhanatai1993@gmail.com>
2022-11-29 22:06:15 +0100

    Update XAS_Inputs.py

--- XAS_Inputs.py
+++ XAS_Inputs.py
@@ -9,9 +9,9 @@ import numpy as np
 #Select the directory where the file is located, as well as TM data
 DataDirectory = '/home/nurekeyev/nurekeyev/2022A8049_backup/process_1/Merge/'
 DataDirectoryTM = '/home/nurekeyev/nurekeyev/'
-SaveFolder = '/home/nurekeyev/nurekeyev/SavedResults/2022_08_23/'
+SaveFolder = '/home/nurekeyev/nurekeyev/SavedResults/2022_08_24/'
 
-ExtraComment = 'test' #Add comment at the end of the generated file
+#ExtraComment = 'test' #Add comment at the end of the generated file
 
 FemtosecondInPls = 6.671 #Femtosends in 1 pls
 TimeZero = 1375 #estimated Time zero in pls
@@ -24,13 +24,14 @@ Bin_Threshold = 0
 
 #Select an optical attenuator value. 0 for 5 uJ of laser energy, -2500 for 2.9 uJ
 Attenuation = np.array([0, -2500])
-# Select the Run Number, in a loop if needed
-RunNumber = 1145445
-
-for i in Attenuation:
-	ExtraComment = i
-	XAS_sorting_tool.sorting_tool(RunNumber, DataDirectory, DataDirectoryTM, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, i, BinSize, Bin_Threshold)
-	XAS_sorting_tool_simple.sorting_tool_simple(RunNumber, DataDirectory, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, i)
+# Select the Run Number, in a loop if needed. WHen using range function, last point isn't included, but first point is
+RunNumber = np.array(range(1145436,1145448,1))
+#RunNumber = np.array([1145436])
+for k in RunNumber:
+	for i in Attenuation:
+		ExtraComment = i
+		XAS_sorting_tool.sorting_tool(k, DataDirectory, DataDirectoryTM, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, i, BinSize, Bin_Threshold)
+		XAS_sorting_tool_simple.sorting_tool_simple(k, DataDirectory, SaveFolder, ExtraComment, FemtosecondInPls, TimeZero, i)
 
 #Full possible inputs:
 #XAS_sorting_tool.sorting_tool(RunNumber,DataDirectory,DataDirectoryTM,SaveFolder,ExtraComment,FemtosecondInPls,TimeZero,Attenuation,BinSize,Bin_Threshold)
