8ce136bda0a41719644d86a601f5f898e696d6b6
Aureliano9 <zhanatai1993@gmail.com>
2023-03-10 17:44:13 +0100

    Update XAS_Inputs_spectra.py

--- XAS_Inputs_spectra.py
+++ XAS_Inputs_spectra.py
@@ -20,9 +20,13 @@ FemtosecondInPls = 6.671 #Femtosends in 1 pls
 TimeZero = 1375 #estimated Time zero in pls
 
 #Select an optical attenuator value. 0 for 5 uJ of laser energy, -2500 for 2.9 uJ
-Attenuation = np.array([0, -2500])
+Attenuation = 0
 # Select the Run Number, in a loop if needed. WHen using range function, last point isn't included, but first point is
-RunNumber = np.array(range(1145436,1145448,1))
+RunNumber = np.array([1145366,1145367,1145390,1145392,1145394,1145395,1145396,1145398,
+		      1145403,1145417,1145418,1145423,1145424,1145425,1145427,1145430,1145431,1145434]) #5 uJ runs
+RunNumber = np.array([1145372,1145373,1145391,1145393,1145397,1145398,1145399,1145404,
+		      1145405,1145406,1145407,1145408,1145419,1145420,1145421,1145422,1145426,1145432,1145433]) #2.9 uJ runs
+#RunNumber = np.array(range(1145436,1145448,1))
 #RunNumber = np.array([1145436])
 for k in RunNumber:
 	for i in Attenuation:
